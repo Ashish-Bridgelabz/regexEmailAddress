e845e16e6d48aaad5670d513cad0a7dd7c35647a
Ashish-Bridgelabz <asaschoudhary49@gmail.com>
2020-03-28 01:31:14 +0530

    ADDED:Displayed the email address varify

---
+++ EmailProblem/validEmail.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -x
+shopt -s extglob
+#WE ARE TAKIING VARIABLE
+emailAddress=0
+#USING FUNCTION
+function validEmailId() {
+	read -p "Enter Email address: " emailId
+	emailRegex="^\w+"
+	if [[ $emailId =~ $emailRegex ]]
+	then
+		echo "Valid"
+	else
+		echo "Invalid"
+	fi
+}
+#CALLING THE FUNCTION
+validEmailId
+
